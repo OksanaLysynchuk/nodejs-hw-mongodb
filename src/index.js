54ac6925e3d7664e58b11891a0ef57d1d54bab42
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-07-12 21:27:37 +0300

    Add MongoDB connection initialization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ require('dotenv').config();
 const { setupServer } = require('./server');
 const { initMongoConnection } = require('./db/initMongoConnection');
 
+const express = require('express');
+
+const app = express();
+
+initMongoConnection();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
