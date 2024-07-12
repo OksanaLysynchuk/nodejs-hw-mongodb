de3b7385f8483453174e7688721efb71ee3e2a8a
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-07-12 22:07:40 +0300

    initMongoConnection.js upd

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:3000/contacts';
 
-const initMongoConnection = async () => {
+async function initMongoConnection() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Connected to MongoDB');
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB', error);
-    process.exit(1);
   }
-};
-module.exports = { initMongoConnection };
+}
+
+module.exports = initMongoConnection;
