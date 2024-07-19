7440443d3e7c2760e099337cb26cc96a51fef7bf
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-07-19 15:10:20 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ const setupServer = () => {
   });
 
   app.use(express.json());
-  app.use('/api', contacts);
+  app.use('/contacts', contacts);
 
   app.use(errorHandler);
   app.use(notFoundHandler);
